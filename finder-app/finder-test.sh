c0cfb49ad004bb0ca7d2cd4db7c534ea08fabcc5
charly <charly@ubuntu.myguest.virtualbox.org>
2024-10-22 12:48:37 -0600

    Assignment 4 part2
    Update output directory for assignment 4

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -7,7 +7,7 @@ set -u
 
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
-WRITEDIR=/tmp/aeld-data
+WRITEDIR=/tmp
 username=$(cat conf/username.txt)
 
 if [ $# -lt 3 ]
@@ -22,7 +22,7 @@ then
 else
 	NUMFILES=$1
 	WRITESTR=$2
-	WRITEDIR=/tmp/aeld-data/$3
+	WRITEDIR=/tmp/$3
 fi
 
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
@@ -60,7 +60,7 @@ done
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
-rm -rf /tmp/aeld-data
+rm -rf /tmp/"$WRITEDIR/${username}*.txt"
 
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
